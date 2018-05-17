92e69e3735831fbf0915de2b64fc3cf70eae677f
Jacob Dayton <jacob@Jacobs-MacBook-Pro.local>
2018-05-17 12:53:07 -0600

    almost done

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Components/Calculator/Calculator'
+
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    )
+  }
+}
+
+export default App;
